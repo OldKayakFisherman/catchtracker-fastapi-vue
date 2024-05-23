d82a4499c8815a378cbd0443e37a43703bcf1333
OldKayakFisherman <94559756+OldKayakFisherman@users.noreply.github.com>
2024-05-23 19:14:15 -0400

    Create main.py

---
+++ backup/main.py
@@ -0,0 +1,35 @@
+from fastapi import FastAPI, Depends
+from starlette.middleware.cors import CORSMiddleware
+from services.config import get_settings
+from api.routes import router
+import uvicorn
+import sys
+
+settings = get_settings()
+
+
+app = FastAPI(
+    title= settings.project_name,
+    docs_url="/api/docs",
+    openapi_url="/api/openapi.json",
+)
+
+
+origins = ["http://localhost:8080"]
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+app.include_router(router, prefix="/api")
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="127.0.0.1", port=5000)
+   
