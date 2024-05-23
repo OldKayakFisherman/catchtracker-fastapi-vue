5f9e39044244a69828c20ca66de50023ef923e0c
OldKayakFisherman <94559756+OldKayakFisherman@users.noreply.github.com>
2024-05-23 19:23:36 -0400

    Create route_tests.py

---
+++ backend/tests/route_tests.py
@@ -0,0 +1,12 @@
+import unittest
+from fastapi.testclient import TestClient
+from main import app
+
+client = TestClient(app)
+
+class TestAPIRoutes(unittest.TestCase):
+
+    def test_lookups(self):
+
+        response = client.get("/api/v1/lookups")
+        self.assertTrue(response.status_code == 200)
