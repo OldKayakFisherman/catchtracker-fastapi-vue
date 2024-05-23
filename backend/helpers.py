da93b9060cd925337946f5b71cecf040e50ee14b
OldKayakFisherman <94559756+OldKayakFisherman@users.noreply.github.com>
2024-05-23 19:16:37 -0400

    Create helpers.py

---
+++ backend/helpers.py
@@ -0,0 +1,33 @@
+
+
+def assign_variables_from_dict(target_class, source_dict: dict) -> object:
+
+    for key in source_dict.keys():
+        setattr(target_class, key, source_dict[key])
+
+    return target_class
+
+def assign_dict_from_variables(source_class) -> dict:
+
+    result = {}
+
+
+    property_exclusion_filter = [
+        '_sa_registry',
+        '_is_protocol',
+        '_sa_class_manager',
+        '_sa_instance_state',
+        'metadata',
+        'registry'
+    ]
+
+
+
+
+    members = [attr for attr in dir(source_class) if not callable(getattr(source_class, attr)) and not attr.startswith("__")]
+
+    for member in members:
+        if member not in property_exclusion_filter:
+            result[member] = getattr(source_class, member)
+
+    return result
