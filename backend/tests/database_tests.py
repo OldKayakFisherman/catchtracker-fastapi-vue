eb05392cd7fe29afb0260caf9480fea16600fccd
OldKayakFisherman <94559756+OldKayakFisherman@users.noreply.github.com>
2024-05-29 19:46:18 -0400

    Update database_tests.py

--- backend/tests/database_tests.py
+++ backend/tests/database_tests.py
@@ -1,22 +1,24 @@
 import unittest
 from database.session import get_db_session
-from database.repositories import LookupRepository, CatchRepository
+from database.models import CatchDetail
+from database.repositories import LookupRepository, CatchRepository, StatRepository
 from tests.utilities import create_test_catch_records
-from tests.utilities import get_db_text_session
+from tests.utilities import get_db_test_session
+from datetime import datetime
 
 class SessionTests(unittest.TestCase):
 
     def test_get_db_session(self):
         self.assertIsNotNone(get_db_session())
 
-class RepositoryTests(unittest.TestCase):
+class LookupRepositoryTests(unittest.TestCase):
 
     def test_get_unique_techniques(self):
 
         catch_repo = CatchRepository()
         lookup_repo = LookupRepository()
 
-        with get_db_text_session() as db:
+        with get_db_test_session() as db:
 
             #Create some test catch records
             raw_catch_records = create_test_catch_records(100)
@@ -29,14 +31,14 @@ class RepositoryTests(unittest.TestCase):
 
             self.assertIsNotNone(unique_records)
             self.assertGreater(len(unique_records), 0)
-            self.assertLessEqual(len(unique_records), 2)
+            self.assertLessEqual(len(unique_records), 4)
 
     def test_get_unique_species(self):
 
         catch_repo = CatchRepository()
         lookup_repo = LookupRepository()
 
-        with get_db_text_session() as db:
+        with get_db_test_session() as db:
 
             #Create some test catch records
             raw_catch_records = create_test_catch_records(100)
@@ -56,7 +58,7 @@ class RepositoryTests(unittest.TestCase):
         catch_repo = CatchRepository()
         lookup_repo = LookupRepository()
 
-        with get_db_text_session() as db:
+        with get_db_test_session() as db:
 
             #Create some test catch records
             raw_catch_records = create_test_catch_records(100)
@@ -77,7 +79,7 @@ class RepositoryTests(unittest.TestCase):
         catch_repo = CatchRepository()
         lookup_repo = LookupRepository()
 
-        with get_db_text_session() as db:
+        with get_db_test_session() as db:
 
             #Create some test catch records
             raw_catch_records = create_test_catch_records(100)
@@ -97,7 +99,7 @@ class RepositoryTests(unittest.TestCase):
         catch_repo = CatchRepository()
         lookup_repo = LookupRepository()
 
-        with get_db_text_session() as db:
+        with get_db_test_session() as db:
 
             #Create some test catch records
             raw_catch_records = create_test_catch_records(100)
@@ -117,7 +119,7 @@ class RepositoryTests(unittest.TestCase):
         catch_repo = CatchRepository()
         lookup_repo = LookupRepository()
 
-        with get_db_text_session() as db:
+        with get_db_test_session() as db:
 
             #Create some test catch records
             raw_catch_records = create_test_catch_records(100)
@@ -132,4 +134,180 @@ class RepositoryTests(unittest.TestCase):
             self.assertGreater(len(unique_records), 0)
             self.assertLessEqual(len(unique_records), 3)
 
+class StatsRepositoryTests(unittest.TestCase):
 
+    def test_ytd_catch_stats_overall(self):
+
+        catch_repo = CatchRepository()
+        stat_repo = StatRepository()
+
+        with get_db_test_session() as db:
+
+            #Create some test catch records
+            raw_catch_records = create_test_catch_records(5)
+
+            for catch_record in raw_catch_records:
+                #assign them current dates
+                catch_record['catch_date'] = datetime.now()
+
+            #Persist those records
+            catch_repo.add_catches(db, raw_catch_records)
+    
+            #test the stat
+            stat_records_count: int = stat_repo.ytd_catch_stats_overall(db)
+
+            self.assertIsNotNone(stat_records_count)
+            self.assertEquals(stat_records_count, 5)
+
+    def test_ytd_catch_stats_species(self):
+
+        catch_repo = CatchRepository()
+        stat_repo = StatRepository()
+
+        with get_db_test_session() as db:
+
+            #Create some test catch records
+            raw_catch_records = create_test_catch_records(5)
+
+            for catch_record in raw_catch_records:
+                #assign them current dates
+                catch_record['catch_date'] = datetime.now()
+                #assign species
+                catch_record['species'] = "Bass"
+
+            #Add some more
+            for catch_record in create_test_catch_records(6):
+                #assign them current dates
+                catch_record['catch_date'] = datetime.now()
+                #assign species
+                catch_record['species'] = "Perch"
+
+                raw_catch_records.append(catch_record)
+                
+            #Add some more
+            for catch_record in create_test_catch_records(10):
+                #assign them current dates
+                catch_record['catch_date'] = datetime.now()
+                #assign species
+                catch_record['species'] = "Crappie"
+
+                raw_catch_records.append(catch_record)
+
+
+            #Persist those records
+            catch_repo.add_catches(db, raw_catch_records)
+    
+            #test the stat
+            stat_records = stat_repo.ytd_catch_stats_by_species(db)
+
+            self.assertIsNotNone(stat_records)
+            
+            for db_record in stat_records:
+                if db_record[0] == "Bass":
+                    self.assertEquals(db_record[1], 5)
+                if db_record[0] == "Perch":
+                    self.assertEquals(db_record[1], 6)
+                if db_record[0] == "Crappie":
+                    self.assertEquals(db_record[1], 10)
+
+    def test_ytd_top_techniques(self):
+
+        catch_repo = CatchRepository()
+        stat_repo = StatRepository()
+
+
+        with get_db_test_session() as db:
+
+            #Create some test catch records
+            raw_catch_records = create_test_catch_records(500)
+
+            for catch_record in raw_catch_records:
+                #assign them current dates
+                catch_record['catch_date'] = datetime.now()            
+
+
+            #Persist those records
+            catch_repo.add_catches(db, raw_catch_records)
+    
+            #test the stat
+            stat_records = stat_repo.ytd_top_techniques(db)
+
+            self.assertIsNotNone(stat_records)
+            self.assertEquals(len(stat_records), 3)
+
+
+    def test_prior_yr_top_techniques(self):
+
+        catch_repo = CatchRepository()
+        stat_repo = StatRepository()
+
+        current_year = datetime.now().year
+
+        with get_db_test_session() as db:
+
+            #Create some test catch records
+            raw_catch_records = create_test_catch_records(500)
+
+            for catch_record in raw_catch_records:
+                #assign them current dates
+                catch_record['catch_date'] = datetime.now().replace(year=(current_year -1))            
+
+            #Persist those records
+            catch_repo.add_catches(db, raw_catch_records)
+    
+            #test the stat
+            stat_records = stat_repo.prior_yr_top_techniques(db)
+
+            self.assertIsNotNone(stat_records)
+            self.assertEquals(len(stat_records), 3)
+
+    def test_ytd_top_baits(self):
+
+        catch_repo = CatchRepository()
+        stat_repo = StatRepository()
+
+
+        with get_db_test_session() as db:
+
+            #Create some test catch records
+            raw_catch_records = create_test_catch_records(500)
+
+            for catch_record in raw_catch_records:
+                #assign them current dates
+                catch_record['catch_date'] = datetime.now()            
+
+
+            #Persist those records
+            catch_repo.add_catches(db, raw_catch_records)
+    
+            #test the stat
+            stat_records = stat_repo.ytd_top_baits(db)
+
+            self.assertIsNotNone(stat_records)
+            self.assertEquals(len(stat_records), 3)
+
+
+    def test_prior_yr_top_baits(self):
+
+        catch_repo = CatchRepository()
+        stat_repo = StatRepository()
+
+        current_year = datetime.now().year
+
+        with get_db_test_session() as db:
+
+            #Create some test catch records
+            raw_catch_records = create_test_catch_records(500)
+
+            for catch_record in raw_catch_records:
+                #assign them current dates
+                catch_record['catch_date'] = datetime.now().replace(year=(current_year -1))            
+
+            #Persist those records
+            catch_repo.add_catches(db, raw_catch_records)
+    
+            #test the stat
+            stat_records = stat_repo.prior_yr_top_baits(db)
+
+            self.assertIsNotNone(stat_records)
+            self.assertEquals(len(stat_records), 3)
