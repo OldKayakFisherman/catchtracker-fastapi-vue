9413d03403c51e9b797af305f159eb2400e8d588
OldKayakFisherman <94559756+OldKayakFisherman@users.noreply.github.com>
2024-06-21 15:02:13 -0400

    Update vite.config.js

--- frontend/catch-tracker-ui/vite.config.js
+++ frontend/catch-tracker-ui/vite.config.js
@@ -1,7 +1,24 @@
+import { fileURLToPath, URL } from 'node:url'
+
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+  ],
+  server: {
+    host: true,
+    port: 8080,
+    strictPort: true,
+    watch: {
+      usePolling: true
+    }
+  },
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
 })
