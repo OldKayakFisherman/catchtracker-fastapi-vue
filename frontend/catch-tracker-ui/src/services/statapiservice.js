b570f1890e5a266ec778d166559b5c0a0b3b6aba
OldKayakFisherman <94559756+OldKayakFisherman@users.noreply.github.com>
2024-06-21 15:10:05 -0400

    Update statapiservice.js

--- frontend/catch-tracker-ui/src/services/statapiservice.js
+++ frontend/catch-tracker-ui/src/services/statapiservice.js
@@ -1,1 +1,19 @@
 
+export default class StatAPIService
+{
+    getStats() {
+
+        axios({
+            method:'post',
+            url:'logout',
+            baseURL: '/stats',
+           })
+           .then(response => {
+              return response.data;
+           })
+           .catch(error => {
+               return -1;
+           });    
+    }
+}
+
