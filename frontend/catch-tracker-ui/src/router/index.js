7ef4be6087f9cb9eee4fb4cfab2861359d6f26e5
OldKayakFisherman <94559756+OldKayakFisherman@users.noreply.github.com>
2024-07-04 18:18:58 -0400

    Update index.js

--- frontend/catch-tracker-ui/src/router/index.js
+++ frontend/catch-tracker-ui/src/router/index.js
@@ -1,18 +1,28 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import IndexView from '../views/IndexView.vue'
-
+import AddCatchView from '../views/AddCatchView.vue'
+import CatchView from '../views/CatchView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
+  linkActiveClass: 'active',
   routes: [
     {
       path: '/',
       name: 'home',
       component: IndexView
     },
-   
+    {
+      path: '/addcatch',
+      name: 'addcatch',
+      component: AddCatchView
+    },
+    {
+      path: '/viewcatches',
+      name: 'viewcatches',
+      component: CatchView
+    },
   ]
 })
 
 export default router
-
